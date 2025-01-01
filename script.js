27fbef0f96c0a4ef52d31914669bbb400245de21
NicholasWMurphy <nickmurphy68@icloud.com>
2025-01-02 08:36:30 +1100

    added the 16x16 grid

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const container = document.querySelector("#grid-container");
+
+
+    for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.border = "2px solid black";
+    container.appendChild(div);
+    }
+
+
+
+
+
+
