8a6879fdd2bb3658c1fc8a99dd381ca2de3742c8
NicholasWMurphy <nickmurphy68@icloud.com>
2025-01-02 12:26:39 +1100

    added the hover mouse event handler

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const container = document.querySelector("#grid-container");
 
     for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    div.style.width = "100px";
-    div.style.height = "100px";
+    div.style.width = "10px";
+    div.style.height = "10px";
     div.style.border = "2px solid black";
     container.appendChild(div);
-    }
 
+    div.onmousemove = (event) => {
+        div.style.backgroundColor = "red";
+    };
+    }
 
 
 
