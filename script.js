534083ff67c2d4abb3b6732928bc4c988ee1244d
NicholasWMurphy <nickmurphy68@icloud.com>
2025-01-31 13:00:56 +1100

    created function to make the cells adjustable based on users input

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-
+const gridSize = 600;
 const container = document.querySelector("#grid-container");
+container.style.width = `${gridSize}px`;
+container.style.height = `${gridSize}px`;
 
 
-    for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.style.width = "10px";
-    div.style.height = "10px";
-    div.style.border = "2px solid black";
-    container.appendChild(div);
 
-    div.onmousemove = (event) => {
-        div.style.backgroundColor = "red";
+    function createGrid(rows, cols) {
+    for (let i = 0; i < rows * cols; i++) {
+    const cells = document.createElement("div");
+    cells.classList.add("cells");
+    cells.style.width = `${gridSize / rows}px`;
+    cells.style.height = `${gridSize / cols}px`;
+    container.appendChild(cells);
+
+    //hover effect
+    cells.onmousemove = () => {
+    cells.style.backgroundColor = "red";
     };
     }
 
+}
 
+ createGrid(16, 16);
+
+         //refreshing canvas when button is pressed
+         const btn = document.querySelector("#refresh-button");
+         btn.addEventListener("click", promptFunction)
+     
+     
+         function promptFunction () {
+             container.innerHTML = '';
+             var answer = parseInt(prompt("How many squares per side would you like for the new grid?", ""));
+             if (answer >= 100) {
+                var answer = parseInt(prompt("Please enter a value below 100", ""));
+                
+             } 
+             createGrid(answer, answer);
+             }
+       
 
 
 
